0f0a11a3ba3fd58299f10c6a3790d49225df27c6
Josue Resende <josue.resende@gmail.com>
2022-06-03 08:39:53 -0300

    WIP

--- src/simple.c
+++ src/simple.c
@@ -196,11 +196,11 @@ int find(int n, int u)
 	}
 	return u;
 }
-void unionbyrank(int n, int u, int v)
+void _union_by_rank(int u, int v, int parent[], int rank[])
 {
 	int x, y;
-	x = find(n, u);
-	y = find(n, v);
+	x = _find(parent, u);
+	y = _find(parent, v);
 	if (rank[x] < rank[y])
 		parent[x] = y;
 	else if (rank[y] < rank[x])
@@ -211,7 +211,29 @@ void unionbyrank(int n, int u, int v)
 		rank[y]++;
 	}
 }
-void kruskal(Aresta *E, int n_nodes, int n_edges)
+int _find(int parent[], int i)
+{
+    if (parent[i] == -1) return i;
+    return _find(parent, parent[i]);
+}
+void _union(int parent[], int x, int y) 
+{
+    int xset = _find(parent, x);
+    int yset = _find(parent, y);
+    parent[xset] = yset;
+}
+void print_rank(int n_nodes) {
+    printf("--\n");
+    printf("P ");
+    for (int i = 0; i < n_nodes; i++) printf("%3d ", parent[i]);
+    printf("\n");
+
+    printf("R ");
+    for (int i = 0; i < n_nodes; i++) printf("%3d ", rank[i]);
+    printf("\n");
+
+}
+void kruskal(Aresta *E, int n_nodes, int n_edges, int parent[], int rank[])
 {
 	quicksort(E, 0, n_edges - 1);
 
@@ -220,27 +242,52 @@ void kruskal(Aresta *E, int n_nodes, int n_edges)
 	int n_node_t = 0;
 	for (int n_edge = 0; n_edge < n_edges; n_edge++)
 	{
-		// printf("%04d | origem: %d | destino: %d | custo: %d \n", n_edge, E[n_edge]._origem, E[n_edge]._destino, E[n_edge]._custo);
-		
         if (n_node_t == (n_nodes - 1)) break;
 		vertice_origem = E[n_edge]._origem;
 		vertice_destino = E[n_edge]._destino;
 		custo = E[n_edge]._custo;
 
-		origem = find(n_nodes, vertice_origem);
-		destino = find(n_nodes, vertice_destino);
+        if (rank == NULL) {
+            origem = _find(parent, vertice_origem);
+            destino = _find(parent, vertice_destino);
+            printf("find(%d) = %d ", vertice_origem, origem);
+            printf("find(%d) = %d ", vertice_destino, destino);
+            printf("\n");
+            if (origem != destino) 
+            {
+                printf("Vertices in different trees. Add edge to tree: Union(%d, %d) \n", origem, destino);
+                _union(parent, origem, destino);
+                n_node_t++;
+            } else {
+                printf("Vertices in the same tree. Skip edge \n");
+            }
+        } else {
+            origem = _find(parent, vertice_origem);
+            destino = _find(parent, vertice_destino);
+            printf("find(%d) = %d ", vertice_origem, origem);
+            printf("find(%d) = %d ", vertice_destino, destino);
+            printf("\n");
+            if (origem != destino) 
+            {
+                printf("Vertices in different trees. Add edge to tree: Union(%d, %d) \n", origem, destino);
+                _union(parent, origem, destino);
+                n_node_t++;
+            } else {
+                printf("Vertices in the same tree. Skip edge \n");
+            }
+        }
 
-		// printf("%04d - find(%d) = %d - find(%d) = %d - custo: %d \n", n_edge, vertice_origem, origem, vertice_destino, destino, custo);
 
         if (origem != destino) 
         {
-            unionbyrank(n_nodes, origem, destino);
+            // printf("Vertices in different trees. Add edge to tree: Union(%d, %d) \n", origem, destino);
+            // unionbyrank(n_nodes, origem, destino);
+            _union(parent, origem, destino);
             n_node_t++;
+        // } else {
+        //     printf("Vertices in the same tree. Skip edge \n");
         }
 	}
-
-	// for (int i = 0; i < n_nodes; i++) printf("%d ", parent[i]);
-	// printf("\n");
 }
 
 int main()
@@ -269,24 +316,24 @@ int main()
                 printf("\n");
             }
         }
+
         int n_nodes = dataset[nb_dataset].grafos[nb_graph].nb_nodes;
+
         parent = malloc(n_nodes * sizeof(int));
-        for (int i = 0; i < n_nodes; i++) parent[i] = i;
+        for (int i = 0; i < n_nodes; i++) parent[i] = -1;
         rank = malloc(n_nodes * sizeof(int));
         for (int i = 0; i < n_nodes; i++) rank[i] = 0;
+
+        // print_rank(n_nodes);
+
         kruskal(
             dataset[nb_dataset].grafos[nb_graph].arestas,
             dataset[nb_dataset].grafos[nb_graph].nb_nodes,
             dataset[nb_dataset].grafos[nb_graph].nb_edges
         );
-        
-        printf("parent ");
-        for (int i = 0; i < n_nodes; i++) printf("%d ", parent[i]);
-        printf("\n");
 
-        printf("rank ");
-        for (int i = 0; i < n_nodes; i++) printf("%d ", rank[i]);
-        printf("\n");
+        print_rank(n_nodes);
+
     }
     
 }
